34661eb8b2a80db0309eb103d108666dd3474483
ODB <apannu@belltechsolutions.ca>
2024-11-05 14:15:24 -0500

    Update script.js
    Added Form A

--- script.js
+++ script.js
@@ -8,16 +8,86 @@ function loadForm() {
     if (formType === "formA") {
         // Load Form A
         dynamicFormContent.innerHTML = `
-            <h2>Sitewalk </h2>
+            <h2>Form A - Worksite and Customer Information</h2>
             <form class="form-container">
-                <div class="form-group">
-                    <label class="form-label" for="fieldA1">Field A1</label>
-                    <input type="text" id="fieldA1" name="fieldA1" class="form-input" required>
-                </div>
-                <div class="form-group">
-                    <label class="form-label" for="fieldA2">Field A2</label>
-                    <input type="text" id="fieldA2" name="fieldA2" class="form-input" required>
-                </div>
+                <fieldset>
+                    <legend>Worksite Information</legend>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="worksitePhone">Phone Number</label>
+                            <input type="text" id="worksitePhone" name="worksitePhone" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="worksiteCity">City</label>
+                            <input type="text" id="worksiteCity" name="worksiteCity" class="form-input">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="worksiteContactName">Contact Name</label>
+                            <input type="text" id="worksiteContactName" name="worksiteContactName" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="worksitePhoneNumber">Phone Number</label>
+                            <input type="text" id="worksitePhoneNumber" name="worksitePhoneNumber" class="form-input">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="poNumber">PO Number</label>
+                            <input type="text" id="poNumber" name="poNumber" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="jobNumber">Job Number</label>
+                            <input type="text" id="jobNumber" name="jobNumber" class="form-input">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="costCentre">Cost Centre</label>
+                            <input type="text" id="costCentre" name="costCentre" class="form-input">
+                        </div>
+                    </div>
+                </fieldset>
+                
+                <fieldset>
+                    <legend>Customer Information</legend>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="customerContactName">Contact Name</label>
+                            <input type="text" id="customerContactName" name="customerContactName" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="customerAddress">Address</label>
+                            <input type="text" id="customerAddress" name="customerAddress" class="form-input">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="customerCity">City</label>
+                            <input type="text" id="customerCity" name="customerCity" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="customerPhoneNumber">Phone Number</label>
+                            <input type="text" id="customerPhoneNumber" name="customerPhoneNumber" class="form-input">
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label class="form-label" for="customerEmail">Email Address</label>
+                            <input type="email" id="customerEmail" name="customerEmail" class="form-input">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="customerProvince">Province</label>
+                            <select id="customerProvince" name="customerProvince" class="form-select">
+                                <option value="">Please select</option>
+                                <option value="Province1">Province 1</option>
+                                <option value="Province2">Province 2</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
                 <button type="submit" class="btn btn-primary">Submit Form A</button>
             </form>
         `;
